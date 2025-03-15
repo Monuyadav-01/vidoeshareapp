12334703b1df017d0e3efde903ba3d26df814201
Monuyadav-01 <monuyadav10012000@gmail.com>
2025-03-15 21:15:18 +0530

    Enhance video publishing functionality: add validation for title, description, and file uploads; implement error handling; and associate video with the owner

--- src/controllers/video.controllers.js
+++ src/controllers/video.controllers.js
@@ -1,7 +1,7 @@
 import mongoose, { isValidObjectId } from "mongoose";
 import { Video } from "../models/video.model.js";
 import { User } from "../models/user.model.js";
-import { ApiError } from "../utils/ApiError.js";
+import { Apierror } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
@@ -12,8 +12,53 @@ const getAllVideos = asyncHandler(async (req, res) => {
 });
 
 const publishAVideo = asyncHandler(async (req, res) => {
-  const { title, description } = req.body;
-  // TODO: get video, upload to cloudinary, create video
+  try {
+    const { title, description } = req.body;
+    // TODO: get video, upload to cloudinary, create video
+    if (!(title || description)) {
+      throw new Apierror(400, "Enter Title and des of video");
+    }
+    const thumbnailLocalPath = req.files?.thumbnail?.[0]?.path || null;
+    if (!thumbnailLocalPath) {
+      throw new Apierror(400, "Thumbnail Local path Not find");
+    }
+    const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
+    if (!thumbnail) {
+      throw new Apierror(401, "Problem uploading Thumbnail");
+    }
+
+    const videoFileLocalPath = req.files?.videoFile?.[0].path || null;
+    if (!videoFileLocalPath) {
+      throw new Apierror(400, "Video Local path Not find");
+    }
+
+    const video = await uploadOnCloudinary(videoFileLocalPath);
+    if (!video) {
+      throw new Apierror(401, "Problem uploading Video");
+    }
+
+    const user = await User();
+    const owner = user.username;
+
+    // make video DB
+
+    const videos = await Video.create({
+      videoFile: video.url,
+      thumbnail: thumbnail.url,
+      title,
+      description,
+      isPublished: true,
+      owner,
+    });
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, videos, "video is uploaded"));
+  } catch (error) {
+    throw new Apierror(401, {
+      error: error.message,
+    });
+  }
 });
 
 const getVideoById = asyncHandler(async (req, res) => {
